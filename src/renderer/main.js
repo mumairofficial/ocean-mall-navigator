0345724e48ffa9cd9f092b1977734099e091610a
mumair <mumairofficial@gmail.com>
2018-11-11 22:41:33 +0500

    mapplic loaded

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,17 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import '@/assets/js/jquery.min.js'
+import '@/assets/js/hammer.min.js'
+import '@/assets/js/jquery.easing.js'
+import '@/assets/js/jquery.mousewheel.js'
+import '@/assets/js/smoothscroll.js'
+import '@/assets/mapplic/mapplic.js'
+
+import '@/assets/css/style.css'
+import '@/assets/css/map.css'
+// import '@/assets/mapplic/mapplic.css'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
