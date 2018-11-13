bed3e9a3079bb167dd4d1b745f116f975790b532
mumair <mumairofficial@gmail.com>
2018-11-13 16:51:06 +0500

    widget minor fixes

--- src/renderer/pages/components/WeatherWidget.vue
+++ src/renderer/pages/components/WeatherWidget.vue
@@ -5,15 +5,23 @@
 <script>
 export default {
   created () {
-    window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
-    window.myWidgetParam.push({id: 15,cityid: '1174872',appid: '9f295bd03cc5a23a696eb64e6db2d150',units: 'metric',containerid: 'openweathermap-widget-15',  });
-    (function() {
-      var script = document.createElement('script');script.async = true;
-      script.charset = "utf-8";
-      script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(script, s);
-    })();
+    this.renderWeatherWidget()
+  },
+  mounted () {
+    this.renderWeatherWidget()
+  },
+  methods: {
+    renderWeatherWidget () {
+      window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
+      window.myWidgetParam.push({id: 15,cityid: '1174872',appid: '9f295bd03cc5a23a696eb64e6db2d150',units: 'metric',containerid: 'openweathermap-widget-15',  });
+      (function() {
+        var script = document.createElement('script');script.async = true;
+        script.charset = "utf-8";
+        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
+      })();
+    }
   }
 }
 </script>
